3fe59c6a2e90cafe47d2115c6717c0a40dca5b4f
crab <joziel848@gmail.com>
2024-05-31 23:22:46 -0400

    fix the parser for lambdas, since they dont take an argument

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,39 @@
 module Main where
 
+import Prelude
+
+import Data.Either
+import Data.Functor
+
+import Control.Monad
+
+import qualified Text.Parsec as Parsec
+import Text.Parsec hiding (parse, runParser)
+
 data Expr = 
     Var Int
-    | Lambda Expr
-    | Apply Expr Expr
+    | Lam Expr
+    | App Expr Expr
+
+runParser str = Parsec.runParser (expr <* eof) () "" str
+
+parse str = case runParser str of
+    (Right r)   -> r
+    (Left e)    -> error $ show e
+
+expr :: (Stream s m Char) => ParsecT s u m Expr
+expr = chainl1 (nonApp <* spaces) (pure App) <?> "expr" where
+    num = do
+        (many1 digit) <?> "name"
+    var = (Var <$> num) <?> "var"
+    lam = do
+        ns <- char '\\' >> spaces >> sepEndBy1 name spaces
+        e <- char '.' >> spaces >> expr
+        (return (foldl1 (.) (fmap Lam ns) e)) <?> "lam"
+    nonApp = (parens expr <|> lam <|> var) <?> "non-app expr"
+    parens = between
+        (char '(' >> spaces)
+        (char ')' >> spaces)
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = putStrLn "Hello, Connard!"
