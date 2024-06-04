f92f84df57e1ceb0819fa2855364140d91eb36a4
crab <joziel848@gmail.com>
2024-06-03 21:11:23 -0400

    mann

--- app/Main.hs
+++ app/Main.hs
@@ -12,8 +12,11 @@ import Text.Parsec hiding (parse, runParser)
 
 data Expr = 
     Var Int
+    | ID Char
     | Lam Expr
     | App Expr Expr
+    | Def Char Expr
+    deriving (Show)
 
 runParser str = Parsec.runParser (expr <* eof) () "" str
 
@@ -21,19 +24,31 @@ parse str = case runParser str of
     (Right r)   -> r
     (Left e)    -> error $ show e
 
+-- TODO: fix that program can start with whitespace
 expr :: (Stream s m Char) => ParsecT s u m Expr
 expr = chainl1 (nonApp <* spaces) (pure App) <?> "expr" where
     num = do
-        (many1 digit) <?> "name"
-    var = (Var <$> num) <?> "var"
+        (many1 digit) <?> "num"
+    sym = do 
+        (alphaNum) <?> "sym"
+    var = (Var . read <$> num) <?> "var"
+    name = (ID <$> sym) <?> "id"
     lam = do
-        ns <- char '\\' >> spaces >> sepEndBy1 name spaces
-        e <- char '.' >> spaces >> expr
-        (return (foldl1 (.) (fmap Lam ns) e)) <?> "lam"
-    nonApp = (parens expr <|> lam <|> var) <?> "non-app expr"
+        body <- char '\\' >> spaces >> expr
+        (return (Lam body)) <?> "lam"
+    def = do
+        char ':' >> spaces
+        name <- sym
+        spaces 
+        body <- brackets expr
+        (return (Def name body)) <?> "def"
+    nonApp = (def <|> name <|> parens expr <|> lam <|> var) <?> "non-app expr"
     parens = between
         (char '(' >> spaces)
         (char ')' >> spaces)
+    brackets = between
+        (char '{' >> spaces)
+        (char '}' >> spaces)
 
 main :: IO ()
-main = putStrLn "Hello, Connard!"
+main = putStrLn $ show (parse ": 9 { \\(a 2) }")
