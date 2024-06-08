fc0790b70320a0e29cd6f519e74c9472fd210e4f
crab <joziel848@gmail.com>
2024-06-07 22:53:43 -0400

    d

--- app/Main.hs
+++ app/Main.hs
@@ -40,11 +40,18 @@ expr = chainl1 (nonApp <* spaces) (pure App) <?> "expr" where
 -- how the index of a variable varies with how nested it is, and keep track of
 -- that!
 subst :: Int -> Expr -> Expr -> Expr
-subst i v (App f x) = App (subst i v f) (subst i v x)
 subst i v (Var n)
   | i == n    = v
-  | otherwise = Var (n+1)
-subst i v (Lam m) = Lam (subst (i+1) v m)
+  | otherwise = Var n
+subst i v (Lam m) = Lam (subst (i+1) (shift 1 0 v) m)
+subst i v (App f x) = App (subst i v f) (subst i v x)
+
+shift :: Int -> Int -> Expr -> Expr
+shift d c (Var k)
+  | k < c    = Var k
+  | k >= c   = Var (k+d)
+shift d c (Lam m) = Lam (shift d (c+1) m)
+shift d c (App f x) = App (shift d c f) (shift d c x)
 
 eval :: Expr -> Expr
 -- β-reduction; perform substitution
@@ -56,7 +63,8 @@ eval (App f x) = case eval f of
 eval e = e
 
 main :: IO ()
-main = putStrLn $ show $ eval $ parse "(\\\\0) 3"
--- (\\0 0)(\\0 0) -> infinite loop
--- (\\\\0)(\\0) -> \\1
--- (\\\\1) 3 -> \\4
+main = putStrLn $ show $ eval $ parse "(\\\\1 2) 1"
+-- (\\0 0)(\\0 0) -> infinite loop WORKS
+-- (\\\\0)(\\0) -> \\1 NO WORKS
+-- (\\\\1) 3 -> \\4 WORKS
+-- (\\\\1 2) 1 -> \\2 1 NO WORKS
